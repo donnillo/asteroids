1ba63fc46196adb9280382fd2c416331049d4cfc
Kiselev Danila <kiselevda3@it.mos.ru>
2025-07-06 02:24:04 +0300

    add clock ticking

--- main.py
+++ main.py
@@ -6,12 +6,15 @@ from constants import Screen
 
 def main():
     screen = pygame.display.set_mode((Screen.WIDTH, Screen.HEIGHT))
+    clock = pygame.time.Clock()
+
     while True:
         screen.fill(Color("black"))
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
